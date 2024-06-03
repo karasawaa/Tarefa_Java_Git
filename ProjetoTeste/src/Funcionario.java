05eccb93f1e3c60bbbf8feb4293b36ff0f567f5c
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-06-03 20:23:57 -0300

    Classe funcionario autalizada

--- ProjetoTeste/src/Funcionario.java
+++ ProjetoTeste/src/Funcionario.java
@@ -1,9 +1,34 @@
 public class Funcionario {
-    
+
     int matricula;
     String nome;
 
     public Funcionario() {
 
+    }
+
+    public Funcionario(int matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        return "Matricula: " + matricula + ", nome: " + nome;
     }
 }
